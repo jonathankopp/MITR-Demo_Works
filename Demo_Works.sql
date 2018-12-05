77f603b241db4117b9cbeabbb117742438bcbf64
Jonathan Koppelman <jonathan.koppelm@gmail.com>
2018-12-05 15:56:45 -0500

    asdf
    asdf

--- Demo_Works.sql
+++ Demo_Works.sql
@@ -36,6 +36,3 @@ CREATE TABLE `Check_Off` (
   `Price` int(10) NULL DEFAULT '0'
 );
 
-INSERT INTO `jobs` (`FirstName`, `LastName`, `Address`, `City`, `PostalCode`, `PGL Insurance WC/Liab/Dis Ins`, `Rodent Control Letter`, `Gas Cut Off boolean`, `Water/Sewer Cut Off`, `SRO Intake Form`, `10 Day Notice Letter`, `Community Board Notice`, `Asbestos Report ACP5/AP21`, `Photographs`, `Landmark Letter`, `PW-1 Application`, `PW-2 App. Demo/Fence`, `Blank Check DOB Filing/Perimit`, `TR1/DS1 from Engineer`, `Tax Map`, `Fence Filing`, `CustomerID`) VALUES
-('Joey', 'PEPE', '29 Cedar St', 'Syosset', 11791, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
-
